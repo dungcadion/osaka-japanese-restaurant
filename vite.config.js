1f71584428dda35cb2dd968a96d4ecbcaff1963f
dungcadion <mahabangulo@gmail.com>
2025-08-17 15:44:48 +0800

    fix: set correct base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/osaka-japanese-restaurant/',
+  base: '/',
   plugins: [react()],
 })
